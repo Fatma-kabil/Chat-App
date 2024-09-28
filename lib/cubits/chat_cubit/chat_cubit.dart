72fd7988316fdc3f05235d37a472ca34aded54f5
Fatma_kabil <fatmakhaled112@gmail.com>
2024-09-28 11:39:01 +0200

    adding logic for chat cubit

--- lib/cubits/chat_cubit/chat_cubit.dart
+++ lib/cubits/chat_cubit/chat_cubit.dart
@@ -1,8 +1,31 @@
 import 'package:bloc/bloc.dart';
+import 'package:chat_app/constant.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 
 part 'chat_state.dart';
 
 class ChatCubit extends Cubit<ChatState> {
   ChatCubit() : super(ChatInitial());
+
+  CollectionReference messages =
+      FirebaseFirestore.instance.collection(kcollectionmessages);
+
+  void sendMessage({required String message, required String email}) {
+    if (message.trim().isNotEmpty) {
+      messages.add({
+        kMessage: message,
+        'created at': DateTime.now(),
+        'id': email,
+      });
+    }
+  }
+
+  void getMessages() {
+    messages.orderBy('created at', descending: true).snapshots().listen(
+      (event) {
+        emit(ChatSuccess());
+      },
+    );
+  }
 }
